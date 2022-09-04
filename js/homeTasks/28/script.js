a72ccece62920f3236c02e51829b8bb913bf9eb2
zvshchanova <Zhanna.Shchanova@gmail.coml>
2022-09-04 08:57:43 +0300

    8 commit

--- js/homeTasks/28/script.js
+++ js/homeTasks/28/script.js
@@ -19,41 +19,45 @@ let divMain = document.createElement('main');
 let divHeader = document.querySelector("header");
 divHeader.after(divMain);
 
-let divMaindiv = document.createElement('div');
-divMain.append(divMaindiv);
-
 let divButton = document.createElement('button');
 divButton.type = "button";
 divButton.innerHTML = "Create";
 divButton.addEventListener("click", clickCreate);
-divMaindiv.append(divButton);
+divButton.id = "butCreate";
+divMain.append(divButton);
 
 function clickCreate() {
+  document.getElementById("butCreate").style.display = 'none';
   const markupForm1 = () => {
     return `<input type="text" class="person" id="fnid" name="firstName" placeholder="First Name" required><br><br>
     <input type="text" class="person" id="lnid" name="lastName" placeholder="Last Name" required><br><br>
-    <input type="text" class="person" id="ageid" name="age" placeholder="Age" required><br><br>`;
+    <input type="text" class="person" id="ageid" name="age" placeholder="Age" required><br><br>
+    <label for "check">Registred</label>
+    <input type="checkbox" class="person" id="checkid" name="check"><br><br>
+    `;
   };
   let tempDiv = document.createElement("form");
   tempDiv.innerHTML = markupForm1();
   divMain.append(tempDiv);
+
   let divButton2 = document.createElement('button');
   divButton2.type = "button";
   divButton2.innerHTML = "Create citizen";
   divButton2.addEventListener("click", clickCreateCitizen);
   divButton2.id = "butCreateCitizen";
-  divMaindiv.append(divButton2);
+  document.querySelector("form").append(divButton2);
 };
 
 function clickCreateCitizen() {
-    if (document.getElementById("fnid").value.length === 0 || document.getElementById("lnid").value.length === 0 || document.getElementById("ageid").value.length === 0){
-        alert("Please fill all field!")
-        return;
-    }
-  
+  if (document.getElementById("fnid").value.length === 0 || document.getElementById("lnid").value.length === 0 || document.getElementById("ageid").value.length === 0) {
+    alert("Please fill all field!")
+    return;
+  }
+
   const citizen1 = new Citizen(document.getElementById("fnid").value, document.getElementById("lnid").value, document.getElementById("ageid").value, false);
   let divp = document.createElement('p');
   divp.innerHTML = `Citizen: ${citizen1.firstName} ${citizen1.lastName}`;
+  divp.id = "infoCitizen";
   divMain.append(divp);
   const markupForm2 = () => {
     return `<input type="text" class="adr" id="countryid" name="country" placeholder="Country" required><br><br>
@@ -68,27 +72,17 @@ function clickCreateCitizen() {
   const butReg = () => {
     if (citizen1.registered == false) {
       let divButton3 = document.createElement('button');
-      //divButton3.innerHTML =`<button type="button" id="butRegister">Register</button>`;
-       divButton3.type = "button";
-       divButton3.innerHTML = "Register";
-       divButton3.id = "butRegister";
+      divButton3.innerHTML =`<button type="button" id="butRegister">Register</button>`;
       divButton3.addEventListener("click", function () {
         const adress1 = new Adress(document.getElementById("countryid").value, document.getElementById("cityid").value, document.getElementById("streetid").value, document.getElementById("hnid").value);
         citizen1.register(adress1);
-        console.log(citizen1.adress.country);
-        let divp2 = document.createElement('p');
-        divp2.innerHTML = `registered at: ${citizen1.adress.country} ${citizen1.adress.city}  ${citizen1.adress.street} ${citizen1.adress.hn}`;
-        divMain.append(divp2);
-        final();
+        divp.innerHTML += `<br> registered at: ${citizen1.adress.country} ${citizen1.adress.city}  ${citizen1.adress.street} ${citizen1.adress.hn}`;
+        console.log(citizen1);
+        document.querySelector('form').remove();
+        document.getElementById("butCreate").style.display = 'block';
       });
-      divMaindiv.append(divButton3)
+      document.querySelector("form").append(divButton3);
     }
   }
-  butReg();
+   butReg();
 };
-
-function final(citizen1) {
-  document.querySelector('form').remove();
-  document.getElementById("butCreateCitizen").remove();
-  document.getElementById("butRegister").remove();
-}
